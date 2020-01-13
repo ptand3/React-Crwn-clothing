3684cd1343c3ca7d80f3c602a4672e7d37688400
PriyaTandon23 <ptandon4@sapient.com>
2020-01-13 14:53:30 +0530

    added cart-icon and cart-dropdown component and implemented cart-reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/logo.svg";
 import "./header.styles.scss";
 import { auth } from "../../firebase/firebase.utils";
-import {connect } from "react-redux";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropDown from "../cart-dropdown/cart-dropdown.component";
 
-const Header = ({ currentUser }) => (  //recieving the updated user value from the UserReducer
+
+const Header = ({ currentUser, hidden }) => (  //recieving the updated user value from the UserReducer
 
     <div className="header">
         <Link className="logo-container" to="/">
@@ -21,12 +24,15 @@ const Header = ({ currentUser }) => (  //recieving the updated user value from t
                     :
                     (<Link className="option" to="/signin">SIGN IN</Link>)
             }
+            <CartIcon />
         </div>
+        {hidden ? null : <CartDropDown />}  {/* By Default the cart icon is hidden if false cart dropdown will show*/}
     </div>
 )
 
-const mapStateToProps = state =>({  //The function is called whenever the store state changes
-    currentUser : state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden }}) => ({  //The function is called whenever the store state changes
+    currentUser,
+    hidden
 });
 
 export default connect(mapStateToProps)(Header);  //Connect is a HOC that takes in the component as an arg and another function to connect the React Component to Redux Store
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,10 @@
 //  contains the object with mutiple reducers as a key whose value is defined in the defined reduers
 import { combineReducers } from "redux";
 import UserReducer from "./user/user-reducer";
+import CartReducer from "./cart/cart-reducers" ;
 
 
 export default combineReducers({
-   user: UserReducer
+   user: UserReducer,
+   cart : CartReducer
 });
